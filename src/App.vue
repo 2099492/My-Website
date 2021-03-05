2ddaaa52d1c8ec11fddc3ea0e746ebb8e32c2134
Laurens van der Bilt <2099492@talnet.nl>
2021-03-05 08:21:28 +0100

    new layout

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <v-app>
     <v-main>
-      <top-bar></top-bar>
+<!--      <top-bar></top-bar>-->
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import TopBar from './components/TopBar'
+// import TopBar from './components/TopBar'
 
 export default {
   name: 'App',
 
   components: {
-    TopBar
+    // TopBar
   },
 
   data () {
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,55 @@
 <template>
-  <v-row justify="center" class="mt-8">
-    <v-col cols="2" class="mr-4">
-      <v-card flat>
-        <v-avatar
-          class="profile"
-          color="grey"
-          size="250"
-        >
-          <v-img
-            :src="require('../assets/d68cefb8-a3b1-4804-a5b1-fb9477d2911a.jpg')"
-          />
-        </v-avatar>
-      </v-card>
-    </v-col>
-    <v-col cols="4">
-      <v-card-title>
-        Laurens van der Bilt
-      </v-card-title>
-      <v-card-text>
-        I am a skilled programmer with a nack for front-end work.
-      </v-card-text>
-    </v-col>
-  </v-row>
+  <div>
+    <v-row justify="center" class="mt-8">
+      <v-col cols="2" class="mr-4">
+        <v-card flat>
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="180"
+          >
+            <v-img
+              :src="require('../assets/d68cefb8-a3b1-4804-a5b1-fb9477d2911a.jpg')"
+            />
+          </v-avatar>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card-title>
+          Laurens van der Bilt
+        </v-card-title>
+        <v-card-text>
+          I am a learning programmer living in Amsterdam and studying at the bit-academy.
+          My hobbies include programming, gaming and hanging out with friends.
+        </v-card-text>
+        <v-card-title>
+          My skills include:
+        </v-card-title>
+        <h4>Coding languages:</h4>
+        <v-list-item>
+          <v-list-item-content>
+            <v-card-text>
+              JavaScript
+            </v-card-text>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <v-card-text>
+              php
+            </v-card-text>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <v-card-text>
+              HTML, CSS
+            </v-card-text>
+          </v-list-item-content>
+        </v-list-item>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
