a9ad8b31bf7414b29a04e0f1bfde16adcddfb187
Laurens van der Bilt <2099492@talnet.nl>
2021-03-05 11:34:25 +0100

    small changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
           <v-avatar
             class="profile"
             color="grey"
-            size="180"
+            size="280"
           >
             <v-img
               :src="require('../assets/d68cefb8-a3b1-4804-a5b1-fb9477d2911a.jpg')"
@@ -15,38 +15,38 @@
         </v-card>
       </v-col>
       <v-col cols="4">
-        <v-card-title>
+        <v-card-title style="font-size: 200%;">
           Laurens van der Bilt
         </v-card-title>
-        <v-card-text>
+        <v-card-text style="font-size: 150%;">
           I am a learning programmer living in Amsterdam and studying at the bit-academy.
           My hobbies include programming, gaming and hanging out with friends.
         </v-card-text>
-        <v-card-title>
-          My skills include:
-        </v-card-title>
-        <h4>Coding languages:</h4>
-        <v-list-item>
-          <v-list-item-content>
-            <v-card-text>
-              JavaScript
-            </v-card-text>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item>
-          <v-list-item-content>
-            <v-card-text>
-              php
-            </v-card-text>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item>
-          <v-list-item-content>
-            <v-card-text>
-              HTML, CSS
-            </v-card-text>
-          </v-list-item-content>
-        </v-list-item>
+<!--        <v-card-title>-->
+<!--          My skills include:-->
+<!--        </v-card-title>-->
+<!--        <h4>Coding languages:</h4>-->
+<!--        <v-list-item>-->
+<!--          <v-list-item-content>-->
+<!--            <v-card-text>-->
+<!--              JavaScript-->
+<!--            </v-card-text>-->
+<!--          </v-list-item-content>-->
+<!--        </v-list-item>-->
+<!--        <v-list-item>-->
+<!--          <v-list-item-content>-->
+<!--            <v-card-text>-->
+<!--              php-->
+<!--            </v-card-text>-->
+<!--          </v-list-item-content>-->
+<!--        </v-list-item>-->
+<!--        <v-list-item>-->
+<!--          <v-list-item-content>-->
+<!--            <v-card-text>-->
+<!--              HTML, CSS-->
+<!--            </v-card-text>-->
+<!--          </v-list-item-content>-->
+<!--        </v-list-item>-->
       </v-col>
     </v-row>
   </div>
@@ -67,3 +67,7 @@ export default {
   }
 }
 </script>
+
+<style>
+
+</style>
