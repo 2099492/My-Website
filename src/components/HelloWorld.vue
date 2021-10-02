905dcccd56fe9f6b75c6a0aa932075f10a5a4493
laurensvanderbilt <2099492@talnet.nl>
2021-10-02 18:36:53 +0200

    fixed overflow

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,45 @@
 <template>
-  <div class="ma-0 pa-0">
-    <v-row justify="center" align="center" class="pa-10 ma-0">
-      <v-col cols="12" lg="2">
-        <v-card flat>
-          <v-avatar
-            class="profile"
-            color="grey"
-            size="280"
-          >
-            <v-img
-              :src="require('../assets/d68cefb8-a3b1-4804-a5b1-fb9477d2911a.jpg')"
-            />
-          </v-avatar>
-        </v-card>
-      </v-col>
-      <v-col cols="12" lg="5">
-        <v-card flat>
-          <v-card-text style="font-size: 200%; font-weight: bold; color: black">
-            Laurens van der Bilt
-          </v-card-text>
-          <v-card-text style="font-size: 150%;">
-            I am a learning programmer living in Amsterdam and studying at the bit-academy.
-            My hobbies include programming, gaming and hanging out with friends.
-          </v-card-text>
-          <v-card-title>
-            You can find me on:
-          </v-card-title>
-          <div class="pl-4">
-            <a href="https://www.linkedin.com/in/laurens-van-der-bilt/" class="pr-2">
-              <v-btn color="primary">
-                LinkedIn
-              </v-btn>
-            </a>
-            <a href="https://github.com/2099492">
-              <v-btn color="secondary">
-                GitHub
-              </v-btn>
-            </a>
-          </div>
-        </v-card>
-      </v-col>
-    </v-row>
-  </div>
+  <v-row justify="center" align="center" class="pa-10 ma-0">
+    <v-col cols="12" lg="2">
+      <v-card flat>
+        <v-avatar
+          class="profile"
+          color="grey"
+          size="280"
+        >
+          <v-img
+            :src="require('../assets/d68cefb8-a3b1-4804-a5b1-fb9477d2911a.jpg')"
+          />
+        </v-avatar>
+      </v-card>
+    </v-col>
+    <v-col cols="12" lg="5">
+      <v-card flat>
+        <v-card-title style="font-size: 200%; color: black; word-break: normal">
+          Laurens van der Bilt
+        </v-card-title>
+        <v-card-text style="font-size: 150%;">
+          I am a learning programmer living in Amsterdam and studying at the bit-academy.
+          My hobbies include programming, gaming and hanging out with friends.
+        </v-card-text>
+        <v-card-title>
+          You can find me on:
+        </v-card-title>
+        <div class="pl-4">
+          <a href="https://www.linkedin.com/in/laurens-van-der-bilt/" class="pr-2">
+            <v-btn color="primary">
+              LinkedIn
+            </v-btn>
+          </a>
+          <a href="https://github.com/2099492">
+            <v-btn color="secondary">
+              GitHub
+            </v-btn>
+          </a>
+        </div>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -58,5 +56,7 @@ export default {
 </script>
 
 <style>
-
+body {
+  overflow: hidden;
+}
 </style>
